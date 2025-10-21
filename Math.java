e385792a2861c38fa6ea8e48a835b60c8eedce64
Jacob Ebrada <ebradajacob@gmail.com>
2025-10-21 12:22:01 +0100

    added multi and divide function

--- Math.java
+++ Math.java
@@ -1,3 +1,20 @@
 public class Math {
     // Functions will be added here by each team member
+
+    /********************************
+    Developer: Jacob Ebrada
+    University ID: 240364687
+    Function: This function takes two inputs as integers and returns the mulitplication
+    ********************************/
+    public int Multi(int num1, int num2){
+        return (num1*num2);
+    }
+    /********************************
+    Developer: Jacob Ebrada
+    University ID: 240364687
+    Function: This function takes two inputs as integers and returns the division
+    ********************************/
+    public int Divide(int num1 int num2){
+        return (num1num2);
+    }
 }
