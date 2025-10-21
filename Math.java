33f15b5e890f499c743d88a000058a8645c82f4d
nahomcs <240136419@aston.ac.uk>
2025-10-21 12:33:34 +0100

    Added the Mod function

--- Math.java
+++ Math.java
@@ -17,10 +17,16 @@ Function: This function takes two inputs as integers, subtracts the second from 
 public int Sub(int x, int y) {
     return x - y;
 }
-    
-    
-    
-    
+
+
+/********************************
+ Developer: Nahom Beraki
+ University ID: 240136419
+ Function: This function takes two inputs as integers and returns 1 or 0 as a result depending on the numbers
+ ********************************/
+public int Sub(int x, int y){
+    return x % y;
+}
     
     
     
