f0355e28cd0e8d34997ebdc2c7908221c902d21c
Ishita Naik <240169990@aston.ac.uk>
2025-10-21 12:10:07 +0100

    Added Power function by Ishita Naik

--- Math.java
+++ Math.java
@@ -1,3 +1,12 @@
 public class Math {
-    // Functions will be added here by each team member
+    /********************************
+    Developer: [Ishita Naik]
+    University ID: [24019990]
+    Function: This function takes two integers as input — the first number is the base and the second one is the exponent.
+    It returns the result of raising the base to the power of the exponent.
+    Example: Power(2, 3) returns 8.
+    ********************************/
+    public int Power(int base, int exponent) {
+        return (int) Math.pow(base, exponent);
+    }
 }
