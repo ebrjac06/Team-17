cffbb710470c64ce29592a65e68a3a713d51ddea
Mohammed <Mohammedt.MO.ta@gmail.com>
2025-10-21 12:32:46 +0100

    Added max and min functions by Talha

--- Math.java
+++ Math.java
@@ -1,7 +1,9 @@
 public class Math {
     // Functions will be added here by each team member
-    //devolper: Mohammed Talha
-    //uni ID: 240207029
+       /********************************
+Developer: Mohammed Talha
+University ID: 240207029 
+********************************/
     public class Main { 
         // Returns the maximum of two integers
         public static int max(int a, int b) {
@@ -13,8 +15,10 @@ public class Math {
             System.out.println("Max: " + max(x, y)); // Output: Max: 20
         }
     }
-    //devolper: Mohammed Talha
-    //uni ID: 240207029
+     /********************************
+Developer: Mohammed Talha
+University ID: 240207029
+********************************/
     public class Main {
         public static int min(int a, int b) {
             return (a < b) ? a : b;
@@ -24,6 +28,5 @@ public class Math {
             System.out.println(min(10, 5));   // Output: 5
             System.out.println(min(-3, 7));   // Output: -3
         }
-    }
-    
+
 }
