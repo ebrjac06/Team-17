2b15579d32b2ba8057ce74635f3557dfd718128d
nahomcs <240136419@aston.ac.uk>
2025-10-21 13:24:18 +0100

    Added the Mod function

--- Math.java
+++ Math.java
@@ -5,7 +5,8 @@ public class Math {
     /********************************
      Developer: Nahom Beraki
      University ID: 240136419
-     Function: This function takes two inputs as integers and returns 1 or 0 as a result depending on the numbers
+     Function: This function takes two integer inputs and returns the remainder when x is divided by y.
+     The remainder can only be one or zero
      ********************************/
     public int Mod(int x, int y){
         return x % y;
