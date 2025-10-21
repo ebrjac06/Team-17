2369200932b792bdc6f18896254d13dcf38fa217
nahomcs <240136419@aston.ac.uk>
2025-10-21 12:51:06 +0100

    Added the Mod function

--- Math.java
+++ Math.java
@@ -1,11 +1,13 @@
 public class Math {
     // Functions will be added here by each team member
+
+
     /********************************
      Developer: Nahom Beraki
      University ID: 240136419
      Function: This function takes two inputs as integers and returns 1 or 0 as a result depending on the numbers
      ********************************/
-    public int Sub(int x, int y){
+    public int Mod(int x, int y){
         return x % y;
     }
 
